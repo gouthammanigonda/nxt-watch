eb115c71f8f0286321ba046ac991ec66314dd946
gouthammanigonda <gouthammanigonda737@gamil.com>
2022-06-18 15:16:27 +0000

    commit 1

---
+++ src/components/Login/index.js
@@ -0,0 +1,10 @@
+import {Component} from 'react'
+import {HeadingLogin} from './styled'
+
+class Login extends Component {
+  render() {
+    return <HeadingLogin>hi</HeadingLogin>
+  }
+}
+
+export default Login
---
+++ src/components/Login/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const HeadingLogin = styled.h1`
+  font-family: 'Roboto';
+  color: lightblue;
+`
